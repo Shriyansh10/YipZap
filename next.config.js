dac303c95c54b50f98e2f7e9d53ecd29dbe10172
Shriyansh Agarwal <62295012+Shriyansh10@users.noreply.github.com>
2025-09-30 18:33:26 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
